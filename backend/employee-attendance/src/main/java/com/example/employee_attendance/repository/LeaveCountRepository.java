27c21143b0cc4b0ce89c1d4826d200fb87fa64d4
Rohit Thite <rohitthite7801@gmail.com>
2025-04-22 23:32:59 +0530

    Updated Leave Request

--- backend/employee-attendance/src/main/java/com/example/employee_attendance/repository/LeaveCountRepository.java
+++ backend/employee-attendance/src/main/java/com/example/employee_attendance/repository/LeaveCountRepository.java
@@ -2,8 +2,6 @@ package com.example.employee_attendance.repository;
 
 import com.example.employee_attendance.model.LeaveCount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- backend/employee-attendance/src/main/java/com/example/employee_attendance/repository/LeaveTypeRepository.java
+++ backend/employee-attendance/src/main/java/com/example/employee_attendance/repository/LeaveTypeRepository.java
@@ -1,10 +1,8 @@
 package com.example.employee_attendance.repository;
 
-import com.example.employee_attendance.model.LeaveCount;
 import com.example.employee_attendance.model.LeaveType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface LeaveTypeRepository extends JpaRepository<LeaveType, Long> {
 
